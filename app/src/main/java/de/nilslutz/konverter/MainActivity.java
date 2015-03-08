1222ac3f4ece50ce76412cafc4223bfada8d5ebf
nlsltz <info@nilslutz.de>
2015-03-08 23:45:43 +0100

    cleaned code

--- app/src/main/java/de/nilslutz/konverter/MainActivity.java
+++ app/src/main/java/de/nilslutz/konverter/MainActivity.java
@@ -8,7 +8,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
